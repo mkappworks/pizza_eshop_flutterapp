71847a93d952c509090c8592b1c4e87b96eab209
mkappworks <76746881+mkappworks@users.noreply.github.com>
2021-03-04 14:20:36 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/pizza_eshop_flutterapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pizza_eshop_flutterapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ test/widget_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:pizza_eshop_flutterapp/main.dart';
+
+void main() {
+  testWidgets('check for container in HomeScreen', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MyApp());
+
+    expect(find.byType(Container), findsOneWidget);
+  });
+}
