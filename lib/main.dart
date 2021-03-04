e3e080b11316b48a51c7dd2beaa89064bdfca101
mkappworks <76746881+mkappworks@users.noreply.github.com>
2021-03-04 14:34:08 +0530

    changes to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'constants.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -18,10 +20,42 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Pizza Shop App'),
-      ),
+      appBar: homeAppBar(),
       body: Container(),
     );
   }
+
+  AppBar homeAppBar(BuildContext context) {
+  return AppBar(
+    backgroundColor: Colors.white,
+    elevation: 0,
+    leading: IconButton(
+      icon: SvgPicture.asset("assets/icons/menu.svg"),
+      onPressed: () {},
+    ),
+    title: RichText(
+      text: TextSpan(
+        style: Theme.of(context)
+            .textTheme
+            .title
+            .copyWith(fontWeight: FontWeight.bold),
+        children: [
+          TextSpan(
+            text: "Punk",
+            style: TextStyle(color: ksecondaryColor),
+          ),
+          TextSpan(
+            text: "Food",
+            style: TextStyle(color: kPrimaryColor),
+          ),
+        ],
+      ),
+    ),
+    actions: <Widget>[
+      IconButton(
+        icon: SvgPicture.asset("assets/icons/notification.svg"),
+        onPressed: () {},
+      ),
+    ],
+  );
 }
