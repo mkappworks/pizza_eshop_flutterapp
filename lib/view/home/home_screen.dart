b77eb7b99588e73dae507c3ac0b231d42a3528a4
Malith Kuruppu <info.mkappworks@gmail.com>
2021-06-24 23:40:03 +0530

    Converted to StatelessWidget

--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -1,35 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:pizza_eshop_flutterapp/controller/pizza_viewmodel.dart';
 
 import '../../utilities/constants.dart';
 import 'components/home_body.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   static String routeName = "/home";
-
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  final model = UserProgressModel();
-
-  @override
-  void initState() {
-    super.initState();
-    _loadMap();
-  }
-
-  void _loadMap() async {
-    await model.fetchMap();
-    setState(() {});
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kBackgroundColor,
-      body: HomeBody(pizzaList: model.pizzaList),
+      body: HomeBody(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton.extended(
         foregroundColor: Colors.white,
