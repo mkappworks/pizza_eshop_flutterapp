075f257f544334a5fa8d3a16452c72f0b2b54b26
Malith Kuruppu <info.mkappworks@gmail.com>
2021-06-24 23:36:59 +0530

    Update Pizza model

--- lib/model/pizza.dart
+++ lib/model/pizza.dart
@@ -1,39 +1,52 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
+
+List<Pizza> pizzaFromJson(String str) =>
+    List<Pizza>.from(json.decode(str).map((x) => Pizza.fromJson(x)));
 
 class Pizza {
-  final String? title;
-  final String? description;
-  final String? image;
-  final String? mediumWeight;
-  final String? mediumCalorie;
-  final double? mediumPrice;
-  final String? largeWeight;
-  final String? largeCalorie;
-  final double? largePrice;
+  final String title;
+  final String description;
+  final String image;
+  final String mediumweight;
+  final String mediumcalorie;
+  final double mediumprice;
+  final String largeweight;
+  final String largecalorie;
+  final double largeprice;
 
   Pizza({
     required this.title,
     required this.description,
     required this.image,
-    required this.mediumWeight,
-    required this.mediumCalorie,
-    required this.mediumPrice,
-    required this.largeWeight,
-    required this.largeCalorie,
-    required this.largePrice,
+    required this.mediumweight,
+    required this.mediumcalorie,
+    required this.mediumprice,
+    required this.largeweight,
+    required this.largecalorie,
+    required this.largeprice,
   });
 
-  factory Pizza.fromJson(Map<String, dynamic> json) {
-    return Pizza(
-      title: json['title'],
-      description: json['description'],
-      image: json['image'],
-      mediumWeight: json['mediumweight'],
-      mediumCalorie: json['mediumcalorie'],
-      mediumPrice: json['mediumprice'],
-      largeWeight: json['largeweight'],
-      largeCalorie: json['largecalorie'],
-      largePrice: json['largeprice'],
-    );
-  }
+  factory Pizza.fromJson(Map<String, dynamic> json) => Pizza(
+        title: json["title"],
+        description: json["description"],
+        image: json["image"],
+        mediumweight: json["mediumweight"],
+        mediumcalorie: json["mediumcalorie"],
+        mediumprice: json["mediumprice"].toDouble(),
+        largeweight: json["largeweight"],
+        largecalorie: json["largecalorie"],
+        largeprice: json["largeprice"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "title": title,
+        "description": description,
+        "image": image,
+        "mediumweight": mediumweight,
+        "mediumcalorie": mediumcalorie,
+        "mediumprice": mediumprice,
+        "largeweight": largeweight,
+        "largecalorie": largecalorie,
+        "largeprice": largeprice,
+      };
 }
