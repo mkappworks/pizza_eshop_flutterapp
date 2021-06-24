e4d0cd207c74b7a653da80945cc0ed047b14bd3e
Malith Kuruppu <info.mkappworks@gmail.com>
2021-06-24 23:40:44 +0530

    Updated with FoodController

--- lib/view/home/components/item_list.dart
+++ lib/view/home/components/item_list.dart
@@ -1,47 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:pizza_eshop_flutterapp/controller/food/food_controller.dart';
 import 'package:pizza_eshop_flutterapp/model/pizza.dart';
 import 'package:pizza_eshop_flutterapp/view/details/details_screen.dart';
 import 'package:pizza_eshop_flutterapp/utilities/constants.dart';
 import 'package:pizza_eshop_flutterapp/utilities/sized_box_functions.dart';
 
 class ItemList extends StatelessWidget {
-  final List<Pizza>? pizzaList;
-
-  ItemList({required this.pizzaList});
+  final FoodController _foodController = Get.find();
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Expanded(
-      child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 15),
-        width: double.infinity,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(30),
-            topRight: Radius.circular(30),
-          ),
-        ),
-        child: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: pizzaList != null
-              ? Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    ...List.generate(
-                        pizzaList!.length,
-                        (index) => ItemDetailsCard(
-                              pizza: pizzaList![index],
-                              size: size,
-                            ))
-                  ],
-                )
-              : SizedBox(),
-        ),
-      ),
-    );
+    //depending on the ListStatus which can be loading or loaded or empty from FoodController the respective widgets are returned
+    return Obx(() {
+      switch (_foodController.getStatus.value) {
+        case ListStatus.loading:
+          return Center(child: CircularProgressIndicator());
+        case ListStatus.loaded:
+          RxList<Pizza> _pizzaList = _foodController.getUsersList;
+          return Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+              ),
+              child: SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      ...List.generate(
+                          _pizzaList.length,
+                          (index) => ItemDetailsCard(
+                                pizza: _pizzaList[index],
+                                size: size,
+                              ))
+                    ],
+                  )),
+            ),
+          );
+        case ListStatus.empty:
+          return Column(
+            children: [
+              Center(
+                child: Text(
+                  'No Pizza Found!',
+                  style: Theme.of(context).textTheme.subtitle1,
+                ),
+              ),
+            ],
+          );
+        default:
+          return Container();
+      }
+    });
   }
 }
 
@@ -67,8 +86,8 @@ class ItemDetailsCard extends StatelessWidget {
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Hero(
-                    tag: pizza.title!,
-                    child: Image.asset(pizza.image!),
+                    tag: pizza.title,
+                    child: Image.asset(pizza.image),
                   ),
                 ),
               ),
@@ -80,12 +99,12 @@ class ItemDetailsCard extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        pizza.title!,
+                        pizza.title,
                         style: Theme.of(context).textTheme.headline2,
                       ),
                       addVerticalSpace(5.0),
                       Text(
-                        '${pizza.mediumWeight} | ${pizza.mediumCalorie}',
+                        '${pizza.mediumweight} | ${pizza.mediumcalorie}',
                       ),
                       addVerticalSpace(20.0),
                       Container(
@@ -121,7 +140,7 @@ class ItemDetailsCard extends StatelessWidget {
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
                         Text(
-                          '\$${pizza.mediumPrice}',
+                          '\$${pizza.mediumprice}',
                           style: Theme.of(context).textTheme.headline2,
                         ),
                         addVerticalSpace(35),
