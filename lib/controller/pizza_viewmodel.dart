23b70b980399ffdc4f679ffd645b9e5d22f849af
Malith Kuruppu <info.mkappworks@gmail.com>
2021-06-24 23:36:18 +0530

    Removed viewmodel

--- lib/controller/pizza_viewmodel.dart
+++ lib/controller/pizza_viewmodel.dart
@@ -1,17 +1,17 @@
-import 'dart:convert';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:pizza_eshop_flutterapp/model/pizza.dart';
+// import 'dart:convert';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+// import 'package:pizza_eshop_flutterapp/model/pizza.dart';
 
-class UserProgressModel with ChangeNotifier {
-  List<Pizza>? pizzaList;
+// class UserProgressModel with ChangeNotifier {
+//   List<Pizza>? pizzaList;
 
-  Future<void> fetchMap() async {
-    final parsedJson = jsonDecode(await _getJson()) as List<dynamic>;
-    pizzaList = parsedJson.map<Pizza>((item) => Pizza.fromJson(item)).toList();
-  }
+//   Future<void> fetchMap() async {
+//     final parsedJson = jsonDecode(await _getJson()) as List<dynamic>;
+//     pizzaList = parsedJson.map<Pizza>((item) => Pizza.fromJson(item)).toList();
+//   }
 
-  Future<String> _getJson() async {
-    return await rootBundle.loadString('assets/sample.json');
-  }
-}
+//   Future<String> _getJson() async {
+//     return await rootBundle.loadString('assets/sample.json');
+//   }
+// }
