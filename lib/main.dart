ff07b6b6942384c31b7ed3704471d3944a8f36a8
Malith Kuruppu <info.mkappworks@gmail.com>
2021-06-24 23:37:33 +0530

    Update main function with Getx

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:pizza_eshop_flutterapp/controller/food/food_controller.dart';
 
 import 'utilities/constants.dart';
 
@@ -6,13 +8,15 @@ import 'view/home/home_screen.dart';
 import 'utilities/routes.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Get.put(FoodController());
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: kPrimaryColor,
