677a2f4d79bf5f5acac8a0f2d51a1d2353b9a79a
Malith Kuruppu <info.mkappworks@gmail.com>
2021-06-25 23:06:22 +0530

    Added getting currentSelectedPizza FoodController

--- lib/view/details/components/item_image.dart
+++ lib/view/details/components/item_image.dart
@@ -1,24 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:pizza_eshop_flutterapp/controller/food/food_controller.dart';
 import 'package:pizza_eshop_flutterapp/model/pizza.dart';
 
 class ItemImage extends StatelessWidget {
-  final Pizza? pizza;
-
-  ItemImage({this.pizza});
+  final FoodController _foodController = Get.find();
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Center(
-      child: Hero(
-        tag: pizza!.title,
-        child: Image.asset(
-          pizza!.image,
-          // height: size.height * 0.3,
-          width: size.width * 0.9,
-          fit: BoxFit.fill,
-        ),
-      ),
+    return Obx(
+      () {
+        Pizza _currentPizza = _foodController.getCurrentSelectedPizza[0];
+        return Center(
+          child: Hero(
+            tag: _currentPizza.title,
+            child: Image.asset(
+              _currentPizza.image,
+              width: size.width * 0.9,
+              fit: BoxFit.fill,
+            ),
+          ),
+        );
+      },
     );
   }
 }
