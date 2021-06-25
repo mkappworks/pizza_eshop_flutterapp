0879b39b7bfd952b042afddc777cbb2d7e5f32ca
Malith Kuruppu <info.mkappworks@gmail.com>
2021-06-25 14:10:36 +0530

    Breaking changes with CategoryController

--- lib/view/home/components/category_item.dart
+++ lib/view/home/components/category_item.dart
@@ -1,48 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:pizza_eshop_flutterapp/controller/item/category_controller.dart';
 
 import 'package:pizza_eshop_flutterapp/utilities/constants.dart';
 
 class CategoryItem extends StatelessWidget {
-  final String? title;
-  final bool isActive;
-  final Function? press;
-  const CategoryItem({
-    Key? key,
-    this.title,
-    this.isActive = false,
-    this.press,
-  }) : super(key: key);
+  final CategoryList category;
+
+  final CategoryController _categoryController = Get.find();
+
+  CategoryItem({required this.category});
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: press as void Function()?,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-        child: Column(
-          children: <Widget>[
-            Text(
-              title!,
-              style: isActive
-                  ? TextStyle(
-                      color: kTextColor,
-                      fontWeight: FontWeight.bold,
-                    )
-                  : TextStyle(fontSize: 12),
-            ),
-            if (isActive)
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 5),
-                height: 3,
-                width: 22,
-                decoration: BoxDecoration(
-                  color: kPrimaryColor,
-                  borderRadius: BorderRadius.circular(10),
-                ),
+    return Obx(() {
+      // ignore: unrelated_type_equality_checks
+      bool isSelected = _categoryController.getCurrentCategory == category;
+
+      return GestureDetector(
+        onTap: () => _categoryController.setCurrentCategory(category),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+          child: Column(
+            children: <Widget>[
+              Text(
+                category.toShortString(),
+                style: (isSelected)
+                    ? TextStyle(
+                        color: kTextColor,
+                        fontWeight: FontWeight.bold,
+                      )
+                    : TextStyle(fontSize: 12),
               ),
-          ],
+              if (isSelected)
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 5),
+                  height: 3,
+                  width: 22,
+                  decoration: BoxDecoration(
+                    color: kPrimaryColor,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
