fa66a4efbc9355c0d68a2698c947d3fd4d1984b3
Malith Kuruppu <info.mkappworks@gmail.com>
2021-06-25 23:07:17 +0530

    Removed unwanted import path

--- lib/view/details/components/order_button.dart
+++ lib/view/details/components/order_button.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+
 import 'package:pizza_eshop_flutterapp/utilities/constants.dart';
 
 class OrderButton extends StatelessWidget {
