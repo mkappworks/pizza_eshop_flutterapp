adcf64227ec428e064cd194287c3e37bc696bee2
Malith Kuruppu <info.mkappworks@gmail.com>
2021-06-25 23:00:51 +0530

    Updated naming of constants

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -3,29 +3,32 @@ import 'package:flutter/material.dart';
 //App colour constants
 const kPrimaryColor = Color(0XFFFF6E40);
 const kSecondaryColor = Color(0xFFB5BFD0);
-const kTextColor = Color(0xFF50505D);
-const kTextLightColor = Color(0xFF6A727D);
-const kTextDarkColor = Color(0xFF000000);
+
+const kPrimaryTextColor = Color(0xFF000000);
+const kSecondaryTextColor = Colors.white;
+const kPrimarySubtitleTextColor = Color(0xFF50505D);
+const kSecondarySubtitleTextColor = Color(0xFF6A727D);
+
 const kBackgroundColor = Color(0xFFEEDEAE5);
 
 //App themes
 const TextTheme kTextThemeDefault = TextTheme(
   headline1: TextStyle(
-      color: kTextDarkColor, fontWeight: FontWeight.bold, fontSize: 30),
+      color: kPrimaryTextColor, fontWeight: FontWeight.bold, fontSize: 30),
   headline2: TextStyle(
-      color: kTextDarkColor, fontWeight: FontWeight.bold, fontSize: 20),
+      color: kPrimaryTextColor, fontWeight: FontWeight.bold, fontSize: 20),
   headline3: TextStyle(
-      color: kTextDarkColor, fontWeight: FontWeight.w700, fontSize: 18),
+      color: kPrimaryTextColor, fontWeight: FontWeight.w700, fontSize: 18),
   headline4: TextStyle(
-      color: kTextDarkColor, fontWeight: FontWeight.bold, fontSize: 16),
+      color: kPrimaryTextColor, fontWeight: FontWeight.bold, fontSize: 16),
   headline5: TextStyle(
-      color: kTextDarkColor, fontWeight: FontWeight.w700, fontSize: 14),
+      color: kPrimaryTextColor, fontWeight: FontWeight.w700, fontSize: 14),
   headline6: TextStyle(
-      color: kTextDarkColor, fontWeight: FontWeight.w700, fontSize: 12),
+      color: kPrimaryTextColor, fontWeight: FontWeight.w700, fontSize: 12),
   subtitle1: TextStyle(
-      color: kTextDarkColor, fontSize: 18, fontWeight: FontWeight.w400),
+      color: kPrimaryTextColor, fontSize: 18, fontWeight: FontWeight.w400),
   subtitle2: TextStyle(
-      color: kTextDarkColor, fontSize: 14, fontWeight: FontWeight.w400),
+      color: kPrimaryTextColor, fontSize: 14, fontWeight: FontWeight.w400),
 );
 
 const kPrimaryAppBarTitle = 'Pizza';
