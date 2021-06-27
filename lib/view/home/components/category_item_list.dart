855f5e6be76fc89c7e2b34abf7e6bc6340ee38cf
Malith Kuruppu <info.mkappworks@gmail.com>
2021-06-27 20:32:43 +0530

    Removed SingleChildScrollView

--- lib/view/home/components/category_item_list.dart
+++ lib/view/home/components/category_item_list.dart
@@ -7,25 +7,23 @@ import 'package:pizza_eshop_flutterapp/view/home/components/category_item.dart';
 class CategoryItemList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          CategoryItem(
-            category: CategoryList.Pizza,
-          ),
-          CategoryItem(
-            category: CategoryList.Chicken,
-          ),
-          CategoryItem(
-            category: CategoryList.Beverages,
-          ),
-          CategoryItem(
-            category: CategoryList.Desserts,
-          ),
-        ],
-      ),
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        CategoryItem(
+          category: CategoryList.Pizza,
+        ),
+        CategoryItem(
+          category: CategoryList.Chicken,
+        ),
+        CategoryItem(
+          category: CategoryList.Beverages,
+        ),
+        CategoryItem(
+          category: CategoryList.Desserts,
+        ),
+      ],
     );
   }
 }
