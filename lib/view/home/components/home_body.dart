97c23c2f64618b753ac6096595d9b99d89f99a17
Malith Kuruppu <info.mkappworks@gmail.com>
2021-06-27 23:47:59 +0530

    Added HomeBody widget

---
+++ lib/view/home/components/home_body.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'package:pizza_eshop_flutterapp/controller/navigation/navigation_controller.dart';
+import 'package:pizza_eshop_flutterapp/view/home/components/checkout_body.dart';
+import 'package:pizza_eshop_flutterapp/view/home/components/order_body.dart';
+import 'package:pizza_eshop_flutterapp/view/home/components/profile_body.dart';
+
+class HomeBody extends StatelessWidget {
+  final NavigationController _navigationController = Get.find();
+  @override
+  Widget build(BuildContext context) {
+    return Obx(() {
+      switch (_navigationController.getCurrentBody.value) {
+        case 0:
+          return OrderBody();
+        case 1:
+          return CheckoutBody();
+        case 2:
+          return ProfileBody();
+        default:
+          return Container();
+      }
+    });
+  }
+}
