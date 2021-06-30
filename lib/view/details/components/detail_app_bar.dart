87eac7289ee715565680957a6cc5f60e77a4a70f
Malith Kuruppu <info.mkappworks@gmail.com>
2021-07-01 02:37:50 +0530

    Updated routing in onTrailingWidgetTap property

--- lib/view/details/components/detail_app_bar.dart
+++ lib/view/details/components/detail_app_bar.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:pizza_eshop_flutterapp/controller/navigation/navigation_controller.dart';
 
 import 'package:pizza_eshop_flutterapp/view/components/custom_app_bar.dart';
+import 'package:pizza_eshop_flutterapp/view/home/home_screen.dart';
 
 class DetailAppBar extends StatelessWidget {
+  final NavigationController _navigationController = Get.find();
   @override
   Widget build(BuildContext context) {
     return CustomAppBar(
@@ -16,7 +19,10 @@ class DetailAppBar extends StatelessWidget {
         Icons.shopping_bag_rounded,
         size: 35,
       ),
-      onTrailingWidgetTap: () => Get.back(),
+      onTrailingWidgetTap: () {
+        _navigationController.setCurrentBody(1);
+        return Get.offAllNamed(HomeScreen.routeName);
+      },
     );
   }
 }
