2572b63aea4bf1cbde1d2d91070f8e7f69150930
Malith Kuruppu <info.mkappworks@gmail.com>
2021-07-01 02:35:07 +0530

    Update OrderController logic

--- lib/controller/order/order_controller.dart
+++ lib/controller/order/order_controller.dart
@@ -4,52 +4,89 @@ import 'package:pizza_eshop_flutterapp/extensions/extensions.dart';
 
 import 'package:pizza_eshop_flutterapp/model/add_on.dart';
 import 'package:pizza_eshop_flutterapp/model/food.dart';
-
 import 'package:pizza_eshop_flutterapp/model/order.dart';
 
 class OrderController extends GetxController {
   RxList<Order> _orderList = <Order>[].obs;
   RxList<AddOn> _currentSelectedAddOn = <AddOn>[].obs;
-  RxList<Food> _currentSelectedFood = <Food>[].obs;
-  RxInt _currentSelectedFoodQuantity = 1.obs;
-  Rx<FoodSize> _currentSelectedFoodSize = FoodSize.medium.obs;
-  RxDouble _currentSelectedFoodPrice = 0.0.obs;
-  RxString _currentSelectedFoodWeightCalorie = "".obs;
+  RxList<Food> _currentSelectedOrder = <Food>[].obs;
+  RxInt _currentSelectedOrderQuantity = 1.obs;
+  Rx<FoodSize> _currentSelectedOrderSize = FoodSize.medium.obs;
+  RxDouble _currentSelectedOrderUnitPrice = 0.0.obs;
+  RxString _currentSelectedFoOrderWeightCalorie = "".obs;
+  RxDouble _orderListTotalPrice = 0.0.obs;
+  RxInt _orderListTotalQuantity = 0.obs;
 
   //GetX Controller function to add a order to the _orderList
-  void setAddOrderToList() {
+  void addOrderToOrderList() {
     Order order = Order(
-      food: _currentSelectedFood[0],
-      size: _currentSelectedFoodSize.value.toFirstLetterUpperCaseOfSize(),
+      food: _currentSelectedOrder[0],
+      size: _currentSelectedOrderSize.value.toFirstLetterUpperCaseOfSize(),
       addOn: [..._currentSelectedAddOn],
-      quantity: _currentSelectedFoodQuantity.value,
-      price: _currentSelectedFoodPrice.value,
+      quantity: _currentSelectedOrderQuantity.value,
+      price: _currentSelectedOrderUnitPrice.value,
     );
 
     List<Order> _tempList = [..._orderList, order];
 
     _orderList.assignAll(_tempList);
 
+    _setOrderListTotalPriceQuantity();
+
     update();
   }
 
-  void setRemoveOrderFromList(Order order) {
+  void removeOrderFromOrderList(Order order) {
     List<Order> _tempList = [..._orderList];
 
     _tempList.remove(order);
 
     _orderList.assignAll(_tempList);
 
+    _setOrderListTotalPriceQuantity();
+
     update();
   }
 
-  void setCurrentSelectedFood(Food food) {
-    _setToInitial();
-    _currentSelectedFood.assign(food);
+  void updateOrderFromOrderList(Order order, FoodQuantity foodQuantity) {
+    List<Order> _tempList = [..._orderList];
+    int _indexOfOrderInOrderList = _tempList.indexOf(order);
 
-    _setCurrentSelectedFoodWeightCalorie(food);
+    int _previousQuantity = _tempList[_indexOfOrderInOrderList].quantity;
 
-    _setCurrentSelectedFoodPrice();
+    if (foodQuantity == FoodQuantity.decrement && _previousQuantity == 1)
+      return;
+
+    if (foodQuantity == FoodQuantity.increment && _previousQuantity == 99)
+      return;
+
+    int _newQuantity = _previousQuantity;
+    (foodQuantity == FoodQuantity.increment) ? _newQuantity++ : _newQuantity--;
+
+    Order _updatedOrder = Order(
+      food: order.food,
+      size: order.size,
+      addOn: order.addOn,
+      quantity: _newQuantity,
+      price: order.price,
+    );
+
+    _tempList[_indexOfOrderInOrderList] = _updatedOrder;
+
+    _orderList.assignAll(_tempList);
+
+    _setOrderListTotalPriceQuantity();
+
+    update();
+  }
+
+  void setCurrentSelectedOrder(Food food) {
+    _initialiseProperties();
+    _currentSelectedOrder.assign(food);
+
+    _setCurrentSelectedOrderWeightCalorie(food);
+
+    _setCurrentSelectedOrderUnitPrice();
 
     update();
   }
@@ -63,72 +100,81 @@ class OrderController extends GetxController {
 
     _currentSelectedAddOn.assignAll(_tempList);
 
-    _setCurrentSelectedFoodPrice();
+    _setCurrentSelectedOrderUnitPrice();
 
     update();
   }
 
-  void setCurrentSelectedFoodQuantity(FoodQuantity foodQuantity) {
+  void setCurrentSelectedOrderQuantity(FoodQuantity foodQuantity) {
     if (foodQuantity == FoodQuantity.decrement &&
-        _currentSelectedFoodQuantity.value == 1) return;
+        _currentSelectedOrderQuantity.value == 1) return;
 
     if (foodQuantity == FoodQuantity.increment &&
-        _currentSelectedFoodQuantity.value == 99) return;
+        _currentSelectedOrderQuantity.value == 99) return;
 
     foodQuantity == FoodQuantity.increment
-        ? _currentSelectedFoodQuantity++
-        : _currentSelectedFoodQuantity--;
+        ? _currentSelectedOrderQuantity++
+        : _currentSelectedOrderQuantity--;
 
-    _setCurrentSelectedFoodPrice();
+    _setCurrentSelectedOrderUnitPrice();
 
     update();
   }
 
-  void setCurrentSelectedFoodSize() {
-    _currentSelectedFoodSize.value =
-        _currentSelectedFoodSize.value == FoodSize.medium
+  void setCurrentSelectedOrderSize() {
+    _currentSelectedOrderSize.value =
+        _currentSelectedOrderSize.value == FoodSize.medium
             ? FoodSize.large
             : FoodSize.medium;
-    _setCurrentSelectedFoodWeightCalorie(_currentSelectedFood[0]);
-    _setCurrentSelectedFoodPrice();
+    _setCurrentSelectedOrderWeightCalorie(_currentSelectedOrder[0]);
+    _setCurrentSelectedOrderUnitPrice();
 
     update();
   }
 
-  void _setCurrentSelectedFoodPrice() {
-    double? _foodPrice = (_currentSelectedFoodSize.value == FoodSize.medium)
-        ? _currentSelectedFood[0].mediumprice
-        : _currentSelectedFood[0].largeprice;
+  void _setCurrentSelectedOrderUnitPrice() {
+    double? _foodPrice = (_currentSelectedOrderSize.value == FoodSize.medium)
+        ? _currentSelectedOrder[0].mediumprice
+        : _currentSelectedOrder[0].largeprice;
 
     double _addOnPrice = 0;
 
     _currentSelectedAddOn.forEach((addOn) => _addOnPrice += addOn.price);
 
-    _currentSelectedFoodPrice.value =
-        (_foodPrice! + _addOnPrice) * _currentSelectedFoodQuantity.value;
+    _currentSelectedOrderUnitPrice.value = _foodPrice! + _addOnPrice;
 
     update();
   }
 
-  void _setCurrentSelectedFoodWeightCalorie(Food currentFood) {
-    if (_currentSelectedFoodSize.value == FoodSize.large) {
-      _currentSelectedFoodWeightCalorie.value =
-          '${currentFood.largeweight} | ${currentFood.largecalorie}';
-    } else {
-      _currentSelectedFoodWeightCalorie.value =
-          '${currentFood.mediumweight} | ${currentFood.mediumcalorie}';
-    }
+  void _setCurrentSelectedOrderWeightCalorie(Food currentFood) =>
+      _currentSelectedFoOrderWeightCalorie.value =
+          (_currentSelectedOrderSize.value == FoodSize.large)
+              ? '${currentFood.largeweight} | ${currentFood.largecalorie}'
+              : '${currentFood.mediumweight} | ${currentFood.mediumcalorie}';
+
+  void _setOrderListTotalPriceQuantity() {
+    List<Order> _tempList = [..._orderList];
+    int _totalQuantity = 0;
+    double _totalPrice = 0.0;
+
+    _tempList.forEach((order) {
+      _totalQuantity += order.quantity;
+      _totalPrice += order.price * order.quantity;
+    });
+
+    _orderListTotalQuantity.value = _totalQuantity;
+    _orderListTotalPrice.value = _totalPrice;
 
     update();
   }
 
-  void _setToInitial() {
-    _currentSelectedFood.clear();
+  void _initialiseProperties() {
+    _currentSelectedOrder.clear();
     _currentSelectedAddOn.clear();
-    _currentSelectedFoodQuantity.value = 1;
-    _currentSelectedFoodSize.value = FoodSize.medium;
-    _currentSelectedFoodPrice.value = 0.0;
-    _currentSelectedFoodWeightCalorie.value = "";
+    _currentSelectedOrderQuantity.value = 1;
+    _currentSelectedOrderSize.value = FoodSize.medium;
+    _currentSelectedOrderUnitPrice.value = 0.0;
+    _currentSelectedFoOrderWeightCalorie.value = "";
 
     update();
   }
@@ -136,16 +182,21 @@ class OrderController extends GetxController {
   //get the all pizza list db
   RxList<Order> get getOrderList => _orderList;
   //get the current selected pizza from the HomeScreen UI
-  RxList<Food> get getCurrentSelectedFood => _currentSelectedFood;
+  RxList<Food> get getCurrentSelectedOrder => _currentSelectedOrder;
   //get the current selected addOnList
   RxList<AddOn> get getCurrentSelectedAddOn => _currentSelectedAddOn;
 
-  RxInt get getCurrentSelectedFoodQuantity => _currentSelectedFoodQuantity;
+  RxInt get getCurrentSelectedOrderQuantity => _currentSelectedOrderQuantity;
 
-  Rx<FoodSize> get getCurrentSelectedFoodSize => _currentSelectedFoodSize;
+  Rx<FoodSize> get getCurrentSelectedOrderSize => _currentSelectedOrderSize;
 
-  RxDouble get getCurrentSelectedFoodPrice => _currentSelectedFoodPrice;
+  RxDouble get getCurrentSelectedOrderUnitPrice =>
+      _currentSelectedOrderUnitPrice;
 
-  RxString get getCurrentSelectedFoodWeightCalorie =>
-      _currentSelectedFoodWeightCalorie;
+  RxString get getCurrentSelectedOrderWeightCalorie =>
+      _currentSelectedFoOrderWeightCalorie;
+
+  RxDouble get getOrderListTotalPrice => _orderListTotalPrice;
+
+  RxInt get getOrderListTotalQuantity => _orderListTotalQuantity;
 }
