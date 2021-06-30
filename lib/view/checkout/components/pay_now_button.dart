ff7c5cd9527eca5cde26e2620b677fe1da7fd67b
Malith Kuruppu <info.mkappworks@gmail.com>
2021-07-01 02:38:19 +0530

    Added PayNowButton() widget

---
+++ lib/view/checkout/components/pay_now_button.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'package:pizza_eshop_flutterapp/controller/order/order_controller.dart';
+
+import 'package:pizza_eshop_flutterapp/utilities/constants.dart';
+
+import 'package:pizza_eshop_flutterapp/view/components/custom_icon_text_button.dart';
+
+class PayNowButton extends StatelessWidget {
+  final OrderController _orderController = Get.find();
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Obx(
+      () {
+        return _orderController.getOrderListTotalQuantity.value != 0
+            ? CustomIconTextButton(
+                width: size.width * 0.35,
+                height: size.height * 0.05,
+                press: () {
+                  Get.snackbar(
+                    "PizzaShop",
+                    "Payment Completed",
+                    maxWidth: size.width * 0.7,
+                    snackPosition: SnackPosition.TOP,
+                    backgroundColor: kSecondaryColor.withOpacity(0.5),
+                    isDismissible: true,
+                    duration: Duration(milliseconds: 1200),
+                  );
+                },
+                label: Text(
+                  "Pay Now",
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline3
+                      ?.copyWith(color: kSecondaryTextColor),
+                ),
+                icon: Icon(
+                  Icons.payment_rounded,
+                  color: Colors.white,
+                ),
+                color: kPrimaryColor,
+                borderRadius: 10.0,
+              )
+            : Container(
+                width: size.width * 0.35,
+                height: size.height * 0.05,
+              );
+      },
+    );
+  }
+}
